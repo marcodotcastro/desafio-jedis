dc0655982b8ae3b414676de7f38dd8f654863e92
Marco Castro <marcodotcastro@gmail.com>
2023-12-07 16:33:58 -0300

    feat(citizen): add all fields

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -3,13 +3,20 @@
 # Table name: citizens
 #
 #  id         :bigint           not null, primary key
+#  active     :boolean          default(TRUE)
+#  cns        :string
+#  cpf        :string
+#  date_birth :date
+#  email      :string
 #  full_name  :string
+#  phone      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Citizen < ApplicationRecord
+  has_one_attached :photo
 
-  validates :full_name, presence: true
+  validates :full_name, :cns, :cpf, :email, :date_birth, :phone, presence: true
 
   def to_s
     full_name
