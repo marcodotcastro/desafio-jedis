4df9052a138931c0a3396fb41cb4eb1ffa4a12ad
Marco Castro <marcodotcastro@gmail.com>
2023-12-08 17:36:22 -0300

    feat: add filter

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ if Rails.env.development?
                             phone: '5568981278826')
 
   citizen1.photo.attach(io: File.open(Rails.root.join('spec', 'support', 'images', 'mulher-1.jpg')), filename: 'mulher-1', content_type: 'image/jpg')
+  Address.create(cep: '	88870-970', city: 'Orleans', neighborhood: 'Centro', street: 'Rua Quinze de Novembro 305 Sala 03', uf: 'SC', citizen: citizen1)
 
   citizen2 = Citizen.create(full_name: 'Aline Machado',
                             cpf: '23872851403',
@@ -19,5 +20,5 @@ if Rails.env.development?
                             active: false)
 
   citizen2.photo.attach(io: File.open(Rails.root.join('spec', 'support', 'images', 'mulher-2.jpg')), filename: 'mulher-2', content_type: 'image/jpg')
-
+  Address.create(cep: '88275-970', city: 'Nova Trento', neighborhood: 'Claraíba', street: 'Estrada Geral de Claraíba, s/n', uf: 'SC', citizen: citizen1)
 end
