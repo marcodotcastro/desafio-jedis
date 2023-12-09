303d3ed3f4a0c4492768f02ab1ef4727b2556b7b
Marco Castro <marcodotcastro@gmail.com>
2023-12-09 13:49:20 -0300

    build: configuration email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,17 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'desafio-jedis-84a8920b76bc.herokuapp.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'desafio-jedis-84a8920b76bc.herokuapp.com',
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
