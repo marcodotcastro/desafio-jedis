2c0eeb19bb39819df247608a75f0341b09b18e79
Marco Castro <marcodotcastro@gmail.com>
2023-12-09 16:27:23 -0300

    fix: test address

--- spec/businesses/senders/changed_spec.rb
+++ spec/businesses/senders/changed_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Senders::Changed do
       end
 
       it 'address' do
-        expect { citizen.update({ full_name: "Gessica", address_attributes: {id: 1, cep: "12347" } }) }.to change { ActionMailer::Base.deliveries.size }.by(2)
+        expect { citizen.update({ full_name: "Gessica", address_attributes: {id: citizen.address.id, cep: "12347" } }) }.to change { ActionMailer::Base.deliveries.size }.by(2)
       end
     end
 
